2261ee22f00a1622ea5e2abacd9fcb9e43badc3f
NsdHSO <nechiforelsamuel@yahoo.com>
2025-10-09 10:43:16 +0300

    feat: 🎸 check the i32

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,25 @@ fn main() {
     app.run().unwrap();
 }
 
-fn check_string_literal (s1: &mut String) {
+#[test]
+fn integer_check() {
+    let t = 3;
+    assert_eq!(check_about_integer(t), 8)
+}
+
+fn check_about_integer(a: i32) -> i32 {
+    let mut t = a;
+    t = 5;
+    println!("{}, {}", a, t);
+    a + t
+}
+
+fn check_string_literal(s1: &mut String) {
     let mut s = "Hello";
     println!("{}", s);
 
     s = "World";
     println!("{}", s);
-
-
 
     s1.push_str(", World!");
     println!("{}", s1);
